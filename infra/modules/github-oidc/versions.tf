dabf69ca24640ef074071100c307e644be3411c7
Kevin Ryan <kevinryan1973@gmail.com>
2026-03-01 12:17:26 +0000

    add lifecycle fixes

--- infra/modules/github-oidc/versions.tf
+++ infra/modules/github-oidc/versions.tf
@@ -1,10 +1,14 @@
 terraform {
+  required_version = ">= 1.5"
+
   required_providers {
     azuread = {
-      source = "hashicorp/azuread"
+      source  = "hashicorp/azuread"
+      version = "~> 3.0"
     }
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
     }
   }
 }
