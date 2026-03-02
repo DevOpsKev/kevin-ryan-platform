79d1fb95000b2963e8c7b70297d86e2868239f13
Kevin Ryan <kevinryan1973@gmail.com>
2026-03-02 22:01:37 +0000

    add site cache rules

--- infra/modules/cloudflare/main.tf
+++ infra/modules/cloudflare/main.tf
@@ -20,3 +20,28 @@ resource "cloudflare_record" "www" {
   proxied = true
   ttl     = 1
 }
+
+resource "cloudflare_ruleset" "cache" {
+  zone_id     = var.zone_id
+  name        = "Cache rules for kevinryan.io"
+  description = "Cache static site and serve stale on origin failure"
+  kind        = "zone"
+  phase       = "http_request_cache_settings"
+
+  rules {
+    action = "set_cache_settings"
+    action_parameters {
+      cache = true
+      edge_ttl {
+        mode    = "override_origin"
+        default = 86400
+      }
+      serve_stale {
+        disable_stale_while_updating = false
+      }
+    }
+    expression  = "(http.host eq \"kevinryan.io\")"
+    description = "Serve stale content on origin failure"
+    enabled     = true
+  }
+}
